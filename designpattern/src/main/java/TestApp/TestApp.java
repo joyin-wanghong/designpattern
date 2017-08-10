c92f690cb1a9f59d74194161f1e5d40b9110fe7e
wanghong <40058121@qq.com>
2017-08-10 16:00:27 +0800

    add

--- designpattern/src/main/java/TestApp/TestApp.java
+++ designpattern/src/main/java/TestApp/TestApp.java
@@ -2,6 +2,10 @@ package TestApp;
 
 import java.util.Vector;
 
+import behavetype.dutychainpattern.DeptManager;
+import behavetype.dutychainpattern.GeneralManager;
+import behavetype.dutychainpattern.Handler;
+import behavetype.dutychainpattern.ProjectManager;
 import behavetype.observerpattern.ConcreteObserver;
 import behavetype.observerpattern.ConcreteSubject;
 import behavetype.observerpattern.Observer;
@@ -122,6 +126,33 @@ public class TestApp extends TestCase {
 		// 第二种写法
 		// TheGreatestSage fish = new Fish(new Bird(sage));
 		fish.move();
+	}
+	
+	public void TestDutychainPattern() {
+		//先要组装责任链
+        Handler h1 = new GeneralManager();
+        Handler h2 = new DeptManager();
+        Handler h3 = new ProjectManager();
+        h3.setSuccessor(h2);
+        h2.setSuccessor(h1);
+        
+        //开始测试
+        String test1 = h3.handleFeeRequest("张三", 300);
+        System.out.println("test1 = " + test1);
+        String test2 = h3.handleFeeRequest("李四", 300);
+        System.out.println("test2 = " + test2);
+        System.out.println("---------------------------------------");
+        
+        String test3 = h3.handleFeeRequest("张三", 700);
+        System.out.println("test3 = " + test3);
+        String test4 = h3.handleFeeRequest("李四", 700);
+        System.out.println("test4 = " + test4);
+        System.out.println("---------------------------------------");
+        
+        String test5 = h3.handleFeeRequest("张三", 1500);
+        System.out.println("test5 = " + test5);
+        String test6 = h3.handleFeeRequest("李四", 1500);
+        System.out.println("test6 = " + test6);
 	}
 
 }
