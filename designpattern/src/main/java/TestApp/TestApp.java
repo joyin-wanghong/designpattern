f13e1532a9b10c8314ce2fc0baff26b852e46246
wanghong <40058121@qq.com>
2017-08-15 10:06:46 +0800

    add

--- designpattern/src/main/java/TestApp/TestApp.java
+++ designpattern/src/main/java/TestApp/TestApp.java
@@ -51,8 +51,26 @@ import factorypattern.factorymethod.ExportHtmlFactory;
 import factorypattern.normalpattern.SendFactory;
 import factorypattern.normalpattern.Sender;
 import junit.framework.TestCase;
+import factorypattern.singlepattern.Resource;
 import factorypattern.singlepattern.SingletonPattern;
+import factorypattern.singlepattern.SomeThing;
 import structuraltype.adapterpattern.classadaperpattern.Adapter;
+import structuraltype.bridgepattern.AbstractRoad;
+import structuraltype.bridgepattern.Bus;
+import structuraltype.bridgepattern.Car;
+import structuraltype.bridgepattern.SpeedWay;
+import structuraltype.bridgepattern.Street;
+import structuraltype.componentpattern.Component;
+import structuraltype.componentpattern.Composite;
+import structuraltype.componentpattern.Leaf;
+import structuraltype.componentpattern.MarketBranch;
+import structuraltype.componentpattern.MarketJoin;
+import structuraltype.facade.ModuleA;
+import structuraltype.facade.ModuleB;
+import structuraltype.facade.ModuleC;
+import structuraltype.facade.ModuleFacade;
+import structuraltype.proxypattern.AbstractObject;
+import structuraltype.proxypattern.ProxyObject;
 import structuraltype.wrapperpattern.Bird;
 import structuraltype.wrapperpattern.Fish;
 import structuraltype.wrapperpattern.Monkey;
@@ -263,65 +281,180 @@ public class TestApp extends TestCase {
 		o.restoreMemento(c.retrieveMemento());
 		System.out.println(o.getState());
 	}
-	
+
 	/**
 	 * 测试状态模式
-	 *@author 汪宏
-	 *@date 2017年8月11日 下午4:31:24 
-	 *@comment
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月11日 下午4:31:24
+	 * @comment
 	 */
 	public void testStatePattern() {
-		//创建状态
+		// 创建状态
 		State state = new ConcreteStateB();
-		//创建环境
+		// 创建环境
 		behavetype.statepattern.Context context = new behavetype.statepattern.Context();
-		//将状态设置到环境中
+		// 将状态设置到环境中
 		context.setState(state);
-		//请求
+		// 请求
 		context.request("test");
 	}
-	
-	
+
 	/**
 	 * 测试策略模式
-	 *@author 汪宏
-	 *@date 2017年8月11日 下午5:03:21 
-	 *@comment
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月11日 下午5:03:21
+	 * @comment
 	 */
 	public void testStrategyPattern() {
-		//选择并创建需要使用的策略对象
-        MemberStrategy strategy = new AdvancedMemberStrategy();
-        //创建环境
-        Price price = new Price(strategy);
-        //计算价格
-        double quote = price.quote(300);
-        System.out.println("图书的最终价格为：" + quote);
+		// 选择并创建需要使用的策略对象
+		MemberStrategy strategy = new AdvancedMemberStrategy();
+		// 创建环境
+		Price price = new Price(strategy);
+		// 计算价格
+		double quote = price.quote(300);
+		System.out.println("图书的最终价格为：" + quote);
 	}
-	
+
 	/**
 	 * 测试类适配器模式
-	 *@author 汪宏
-	 *@date 2017年8月14日 上午10:13:38 
-	 *@comment
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月14日 上午10:13:38
+	 * @comment
 	 */
 	public void testClassAdaperPattern() {
 		Adapter adapter = new Adapter();
-		//本来Adaptee 是没有 sampleOperation2 功能的，通过继承的实现使 adapter 具有sampleOperation2的接口功能。
+		// 本来Adaptee 是没有 sampleOperation2 功能的，通过继承的实现使 adapter 具有sampleOperation2的接口功能。
 		adapter.sampleOperation1();
 		adapter.sampleOperation2();
 	}
-	
+
 	/**
 	 * 测试对像的适配器模式
-	 *@author 汪宏
-	 *@date 2017年8月14日 上午10:20:24 
-	 *@comment
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月14日 上午10:20:24
+	 * @comment
 	 */
 	public void testObjectAdaperPattern() {
 		Adapter adapter = new Adapter();
-		//通过委派的形式使adapter 具有 sampleOperation2 功能
+		// 通过委派的形式使adapter 具有 sampleOperation2 功能
 		adapter.sampleOperation1();
 		adapter.sampleOperation2();
+	}
+
+	/**
+	 * 测试桥接模式
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月14日 上午10:54:02
+	 * @comment
+	 */
+	public void testBridgePattern() {
+		AbstractRoad speedWay = new SpeedWay();
+		speedWay.aCar = new Car();
+		speedWay.run();
+
+		AbstractRoad street = new Street();
+		street.aCar = new Bus();
+		street.run();
+	}
+
+	/**
+	 * 测试组合模式
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月14日 下午2:36:29
+	 * @comment
+	 */
+	public void testComponentPattern() {
+		// 构造根节点
+		Composite rootComposite = new Composite();
+		rootComposite.name = "根节点";
+
+		// 左节点
+		Composite compositeLeft = new Composite();
+		compositeLeft.name = "左节点";
+
+		// 构建右节点，添加两个叶子几点，也就是子部件
+		Composite compositeRight = new Composite();
+		compositeRight.name = "右节点";
+		Leaf leaf1 = new Leaf();
+		leaf1.name = "右-子节点1";
+		Leaf leaf2 = new Leaf();
+		leaf2.name = "右-子节点2";
+		compositeRight.add(leaf1);
+		compositeRight.add(leaf2);
+
+		// 左右节点加入 根节点
+		rootComposite.add(compositeRight);
+		rootComposite.add(compositeLeft);
+		// 遍历组合部件
+		rootComposite.eachChild();
+	}
+
+	/**
+	 * 测试组合模式2
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月14日 下午2:48:29
+	 * @comment
+	 */
+	public void testComponentPattern2() {
+		MarketBranch rootBranch = new MarketBranch("总店");
+		MarketBranch qhdBranch = new MarketBranch("秦皇岛分店");
+		MarketJoin hgqJoin = new MarketJoin("秦皇岛分店一海港区加盟店");
+		MarketJoin btlJoin = new MarketJoin("秦皇岛分店二白塔岭加盟店");
+		qhdBranch.add(hgqJoin);
+		qhdBranch.add(btlJoin);
+		rootBranch.add(qhdBranch);
+		rootBranch.PayByCard();
+	}
+
+	/**
+	 * 测试门面模式（外观模式）
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月14日 下午3:09:46
+	 * @comment
+	 */
+	public void testFacadePattern() {
+		ModuleFacade mf = new ModuleFacade();
+		mf.a1();
+		mf.b1();
+		mf.c1();
+	}
+
+	/**
+	 * 测试enum实现的单例模式
+	 * 
+	 * @author 汪宏
+	 * @date 2017年8月14日 下午6:32:12
+	 * @comment
+	 */
+	public void testEnumSinglePattern() {
+		SomeThing e = SomeThing.INSTANCE;
+		Resource resource = e.getInstance();
+
+		System.out.println(resource.hashCode());
+
+		Resource resource2 = e.getInstance();
+
+		System.out.println(resource2.hashCode());
+	}
+
+	/**
+	 * 测试结构型-代理模式
+	 * 
+	 *@author 汪宏
+	 *@date 2017年8月15日 上午10:02:44 
+	 *@comment
+	 */
+	public void testProxyPattern() {
+		AbstractObject obj = new ProxyObject();
+		obj.operation();
 	}
 
 }
